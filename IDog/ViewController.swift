bc9284980bdb73aad247356e523162cc64848491
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2020-08-31 17:51:07 -0300

    finally app

--- IDog/ViewController.swift
+++ IDog/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var legendaResultado: UILabel!
+    @IBOutlet weak var campoIdadeCachorro: UITextField!
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+        let idade = Int(campoIdadeCachorro.text!)! * 7
+        legendaResultado.text = "A idade em humanos é: " + String(idade)
+        campoIdadeCachorro.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("meu primeiro app")
     }
-
-
 }
 
