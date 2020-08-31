b7760377b98a3cae54591394acbe956d32271778
Luiz Carlos Rocha Correa <luiz.correa.dev@gmail.com>
2020-08-31 12:17:29 -0300

    Initial Commit

---
+++ IDog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IDog
+//
+//  Created by Luiz Correa on 31/08/20.
+//  Copyright © 2020 Luiz Correa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
